ae4c36eb457c10c8f6483c1a485c601edd77de10
rdragan <rdragan@student.42heilbronn.de>
2023-05-30 19:49:45 +0200

    sorting 5 properly

--- src/sorting/sort_up_to50.c
+++ src/sorting/sort_up_to50.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 17:22:10 by rdragan           #+#    #+#             */
-/*   Updated: 2023/05/30 15:24:33 by rdragan          ###   ########.fr       */
+/*   Updated: 2023/05/30 19:48:47 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,34 +35,19 @@ Sorts a stack composed of three nodes.
 */
 void	sort_up_to50(t_stack *stack_a, t_stack *stack_b)
 {
-	int	length;
-	int	max_in_stack_b;
+	int	init_length;
 
-	length = stack_a->length;
-	while (stack_is_sorted(stack_a) == 0)
+	init_length = stack_a->length;
+	while (stack_a->length > 5)
 	{
-		while (stack_a->length > 3)
-		{
-			if (stack_a->head->data < length - 3)
-				push(stack_a, stack_b, 1);
-			else
-				rotate(stack_a, 1);
-		}
-		if (stack_is_sorted(stack_a) == 0)
-			sort3(stack_a, length - 2);
-		while (stack_b->length > 1)
-		{
-			max_in_stack_b = get_max_in_stack(stack_b);
-			if (stack_b->head->data < stack_b->head->prev->data)
-				swap(stack_b, 0);
-			if (stack_b->head->data < max_in_stack_b / 2)
-				rotate(stack_b, 0);
-			else
-				push(stack_a, stack_b, 0);
-		}
+		ft_printf(1, "h: %d\t init_len: %d\n", stack_a->head->data, init_length);
+		// sleep(1);
+		if (stack_a->head->data < init_length - 5)
+			push(stack_a, stack_b, 1);
+		else
+			rotate(stack_a, 1);
 	}
-	if (stack_b->head->data < stack_b->tail->data)
-		swap(stack_b, 0);
-	push(stack_a, stack_b, 0);
-	push(stack_a, stack_b, 0);
+	sort5(stack_a, stack_b, init_length - 3);
+	print_stack(*stack_a, 1);
+	print_stack(*stack_b, 0);
 }
